6d53842654d28e7d8888b02be739fe52cd575e39
marfa65 <marfa.gonzalez@hotmail.com>
2021-12-16 17:18:05 -0300

    db conn test1

--- index.js
+++ index.js
@@ -13,6 +13,28 @@ const server = require("./src/app.js");
 //   console.log(`Servidor listo en la URL ${url}`);
 // });
 
+const { Client } = require("pg");
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+client.connect();
+
+client.query(
+  "SELECT table_schema,table_name FROM information_schema.tables;",
+  (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    client.end();
+  }
+);
+
 server.listen({ port: process.env.PORT || 3001 }, () => {
   console.log("%s listening at 3001"); // eslint-disable-line no-console
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -11,7 +11,7 @@ router.get("/api", async (req, res) => {
   const info = await axios.get("https://api.TheDogAPI.com/v1/breeds", {
     headers: { "x-api-key": "ae0a2c56-b39f-4473-8631-391eaf562816" },
   });
-  console.log("infooo", info.data);
+  // console.log("infooo", info.data);
   res.json(info.data);
   //   res.send("Hola");
 });
