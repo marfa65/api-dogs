69c7f5f1db2bd2196901865a626d6c41cec114a7
marfa65 <marfa.gonzalez@hotmail.com>
2021-12-18 16:49:44 -0300

    config

--- src/db.js
+++ src/db.js
@@ -1,39 +1,37 @@
-require('dotenv').config();
+require("dotenv").config();
 const { Sequelize } = require("sequelize");
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
-const {
-  DB_USER, DB_PASSWORD, DB_HOST,PORT
-} = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, PORT } = process.env;
 
-function dbConection(){
+function dbConection() {
+  let connection_DB = null;
 
-  let connection_DB= null;
-
-if (process.env.DATABASE_URL){ // verifying if exist herolku env
-  console.log('CONECCION DB HEROKU')
-  connection_DB = new Sequelize(process.env.DATABASE_URL, {
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
+  if (process.env.DATABASE_URL) {
+    // verifying if exist herolku env
+    console.log("CONECCION DB HEROKU");
+    connection_DB = new Sequelize(process.env.DATABASE_URL, {
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
       },
-    },
-  });
-
-}else{
-  console.log('CONECCION DB LOCAL ')
-  connection_DB = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${PORT}/dogs`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
-}
-return connection_DB
-
+    });
+  } else {
+    console.log("CONECCION DB LOCAL ");
+    connection_DB = new Sequelize(
+      `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${PORT}/dogs`,
+      {
+        logging: false, // set to console.log to see the raw SQL queries
+        native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+      }
+    );
+  }
+  return connection_DB;
 }
 const sequelize = dbConection();
-
 
 // connecting DB
 sequelize
@@ -45,24 +43,29 @@ sequelize
     console.error("Unable to connect to the database:", err);
   });
 
-  const basename = path.basename(__filename);
+const basename = path.basename(__filename);
 
 const modelDefiners = [];
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
-fs.readdirSync(path.join(__dirname, '/models'))
-  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+fs.readdirSync(path.join(__dirname, "/Models"))
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+  )
   .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, '/models', file)));
+    modelDefiners.push(require(path.join(__dirname, "/Models", file)));
   });
 
 // Injectamos la conexion (sequelize) a todos los modelos
-modelDefiners.forEach(model => model(sequelize));
+modelDefiners.forEach((model) => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+let capsEntries = entries.map((entry) => [
+  entry[0][0].toUpperCase() + entry[0].slice(1),
+  entry[1],
+]);
 sequelize.models = Object.fromEntries(capsEntries);
-
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
